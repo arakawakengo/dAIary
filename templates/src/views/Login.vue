05d172b0428c45eb7709418a968cb892daae028c
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-05-23 13:59:14 +0900

    ログイン失敗メッセージの表示を追加

--- templates/src/views/Login.vue
+++ templates/src/views/Login.vue
@@ -17,6 +17,13 @@
           Don't have an account? <router-link class="text-indigo-600 hover:text-indigo-500" to="/register">Sign in</router-link>
         </p>
       </div>
+        <transition name="modal">
+          <div class="modal" v-if="errorMessage">
+            <p>{{ errorMessage }}</p>
+            <p> Please, try again. </p>
+            <button class="modal-button" @click="clearError">OK</button>
+          </div>
+        </transition>
     </div>
   </div>
 </template>
@@ -29,6 +36,7 @@
       return {
         email: '',
         password: '',
+        errorMessage: '',
       };
     },
     methods: {
@@ -46,9 +54,46 @@
   } catch (error) {
     console.error('Login failed.');
     console.error(error.response.data);
+    this.errorMessage = error.response.data.error || 'ログインに失敗しました';
   }
+},
+clearError() {
+  this.errorMessage = '';
 },
     },
   };
   </script>
+
+
+  <style scoped>
+    .modal {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: white;
+      padding: 1em 2em;
+      border-radius: 7px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      z-index: 1000;
+      border: 0.5px solid #333;
+    }
+
+    .modal-button {
+      padding: 0.4em 1em;
+      border-radius: 3px;
+      border: 0.5px solid #333; /* ボタンに枠を追加 */
+      cursor: pointer;
+      background-color: #eee;
+      margin-top: 15px;
+      margin-bottom: 0px;
+    }
+    
+    .modal-enter-active, .modal-leave-active {
+      transition: opacity .05s;
+    }
+    .modal-enter, .modal-leave-to {
+      opacity: 0;
+    }
+    </style>
   
