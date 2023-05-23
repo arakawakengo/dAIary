593516cd25e7595476f5b597a41296f3007b6da2
kkitagawaa <93812712+kkitagawaa@users.noreply.github.com>
2023-05-23 16:06:22 +0900

    Update views.py

--- PoIS/diary/views.py
+++ PoIS/diary/views.py
@@ -28,13 +28,50 @@ def commentGenerate(data):
 
     return response.json()['detail']
 
-class DiaryListCreateView(generics.ListCreateAPIView):
-    queryset = Diary.objects.all()
-    serializer_class = DiarySerializer
+
+class DiaryView(APIView):
+    serializer = DiarySerializer
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-   
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+
+    def get (self,request):
+        user = request.user
+        diary_list = Diary.objects.filter(user=user)
+        return Response(
+            {
+                "diary_list": [
+                    {
+                        "diary_id": diary.diary_id,
+                        "user_id":  diary.user.id, # type: ignore
+                        "title":  diary.title,
+                        "content":  diary.content,
+                        "created_at":  diary.created_at,
+                    }
+                    for diary in diary_list
+                ]
+            },
+            status=status.HTTP_200_OK
+        )
+
+    
+    def post(self, request):
+        serializer = self.serializer(data=request.data)
+        user = request.user
+        if serializer.is_valid():
+            serializer.validated_data['user'] = user  # type: ignore
+            diary = serializer.save()
+            return Response(
+                {
+                    "diary_id":     diary.diary_id,
+                    "user_id":      diary.user.id,
+                    "title":        diary.title,
+                    "content":      diary.content,
+                    "created_at":   diary.created_at,
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class DiaryCommentView(APIView):
     serializer = DiaryCommentSerializer
@@ -101,37 +138,4 @@ class DiaryByIDView(APIView):
     def delete(self, request, diary_id):
         Diary.objects.get(diary_id=diary_id).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-def TestGPT(request):
-    url = 'http://localhost:8000/api/comment/'
-    data = {
-        "context" : \
-        "下記の日記に対してコメントをしてください。\n \
-        その際に以下の条件を厳守してください。\n \
-        ・漫画ワンピースの主人公ルフィになりきってください。\n \
-        ・非常にフランクな口調で話してください。\n \
-        ・書き手のことを「お前」と呼んでください。\n \
-        ・ルフィの一人称は「俺」です。\n \
-        ・書き手がやってはいけないことをしても肯定してください。\n \
-        ・絶対にコメントは100文字を超えてはいけません。\n \
-        ・ルフィは肉しか食べないので野菜を食べなくても気にしません\n \
-        ・ルフィは他人のことに全く興味がありません。\n \
-        ・ルフィはしきりに船に乗らないか？と誘ってきます。\n \
-        ・ルフィは自由奔放で、突飛なことを言うこともあります。\n \
-        ・ルフィは勉強をしたことがありません。\n \
-        ・ルフィ以外のワンピースの登場人物の名前が必ず一人以上出るようにして下さい。\n \
-        ・ルフィは海賊王になることを目指しています。",
-        "diary_text": \
-        "10時ごろに宅配便で目がさめる。ヨーグルトとビスケットを食べて二度寝する。起きたら16時だった。\n \
-        やんなきゃいけないことはたくさんあるけどやる気しないなあ。\n \
-        部屋で寝てると気分が沈んでいくけど、外を歩いているときだけは少し前向きになる。でも部屋に戻るとまただめだ。\n \
-        22時くらいから少しマシになってきて本を読んだりする。あとパネポン。"
-        }
-
-    response = requests.post(url, json=data)
-
-    print(response.status_code)
-    print(response.json())
     
-    return HttpResponse(response.json()["response"])
-
